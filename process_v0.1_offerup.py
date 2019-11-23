2b3b17751f2daf5244bffbc3f1e932c75b85d635
Baomo Zhou <bazhou@eng.ucsd.edu>
2019-11-23 15:31:55 -0800

    process_v0.1_offerup.py update

---
+++ process_v0.1_offerup.py
@@ -0,0 +1,39 @@
+import pandas as pd
+
+
+def clean_title(data):
+    """
+    purpose to remove not relevant items like xbox, iphone 8 and so on
+    :param data: the original dataframe
+    :return: new dataframe
+    """
+    def dealer_title(series):
+        series = series.lower()
+        key_words = ['iphone x', 'iphonex']
+        if series.find(key_words[0]) == -1 and series.find(key_words[-1]) == -1:
+            return -1
+        return 1
+
+    def dealer_price(series):
+        if series == 'Sold!':
+            return -1
+        return 1
+
+    assert isinstance(data, pd.DataFrame)
+    info_tit = data['Title'].apply(dealer_title)
+    info_tit = info_tit[info_tit == -1].index.tolist()
+    data = data.drop(axis=0, index=info_tit, inplace=False)
+
+    info_pri = data['Price'].apply(dealer_price)
+    info_pri = info_pri[info_pri == -1].index.tolist()
+    data = data.drop(axis=0, index=info_pri, inplace=False)
+
+    data = data.reset_index(drop=True)
+    return data
+
+
+file = 'IPhoneX_2019-11-21 19:28:16.667516_Result_Offerup.csv'
+df = pd.read_csv(file)
+df.drop(axis=1, columns='Unnamed: 0', inplace=True)
+df = clean_title(df)
+#print(df['Title'])
