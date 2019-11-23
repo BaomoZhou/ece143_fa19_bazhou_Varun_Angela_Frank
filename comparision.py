9651b9ad4bfe3389259349449bc1b96a65b2f215
Frank Ni <nycorz@wireless-169-228-114-43.ucsd.edu>
2019-11-23 15:56:12 -0800

    update

--- comparision.py
+++ comparision.py
@@ -1,25 +1,30 @@
-# -*- coding: utf-8 -*-
-import pandas as pd
-
-sold_id = []
-new_id = []
-same_id = []
-
-data_yst = pd.read_csv('test_yst.csv')
-data_td = pd.read_csv('test_td.csv')
-
-ID_yst = data_yst['Item ID']
-ID_td = data_td['Item ID']
-
-for item in ID_td:
-    if item not in ID_yst.values:
-        new_id.append(item)
-    else:
-        same_id.append(item)
-for item in ID_yst:
-    if item not in ID_td.values:
-        sold_id.append(item)
+def compare_csv(yst,td):
+    '''
     
-print(sold_id)
-print(new_id)
-print(same_id)
+    :param yst: address of yesterday's csv
+    :type yst: str
+    :param td: address of today's csv
+    :type td: str
+    '''
+    
+    sold_id = []
+    new_id = []
+    same_id = []
+    
+    data_yst = pd.read_csv(yst)
+    data_td = pd.read_csv(td)
+    
+    ID_yst = data_yst['Item ID']
+    ID_td = data_td['Item ID']
+    
+    for item in ID_td:
+        if item not in ID_yst.values:
+            new_id.append(item)
+        else:
+            same_id.append(item)
+        
+    for item in ID_yst:
+        if item not in ID_td.values:
+            sold_id.append(item)
+        
+    return sold_id,new_id,same_id
